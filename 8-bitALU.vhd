096238bd4e8fb28a599e793ac1d2094cb40de41a
Noob_guy@69 <147694315+lol-coder69@users.noreply.github.com>
2026-02-27 02:53:06 +0530

    Update 8-bitALU.vhd

--- 8-bitALU.vhd
+++ 8-bitALU.vhd
@@ -1,62 +1,93 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity alu_8bit is
-Port(
-a:in std_logic_vector(7 downto 0);
-b:in std_logic_vector(7 downto 0);
-opcode:in std_logic_vector(3 downto 0);
-res:out std_logic_vector(15 downto 0);
-zf:out std_logic;
-sf:out std_logic
-);
-end alu_8bit;
+entity tb_alu_8bit is
+end tb_alu_8bit;
 
-architecture Behaviour of alu_8bit is
-signal tempRes:std_logic_vector(15 downto 0);
+architecture Behavior of tb_alu_8bit is
+    component alu_8bit
+    port(
+         a      : in  std_logic_vector(7 downto 0);
+         b      : in  std_logic_vector(7 downto 0);
+         opcode : in  std_logic_vector(3 downto 0);
+         res    : out std_logic_vector(15 downto 0);
+         zf     : out std_logic;
+         sf     : out std_logic;
+         cf     : out std_logic
+        );
+    end component;
+    signal a      : std_logic_vector(7 downto 0) := (others => '0');
+    signal b      : std_logic_vector(7 downto 0) := (others => '0');
+    signal opcode : std_logic_vector(3 downto 0) := (others => '0');
+    signal res    : std_logic_vector(15 downto 0);
+    signal zf     : std_logic;
+    signal sf     : std_logic;
+    signal cf     : std_logic;
+    constant clock_period : time := 100 ps;
+    signal clk : std_logic := '0';
 begin
-process(a,b,opcode)
-begin
+    uut: alu_8bit port map (
+          a => a, 
+          b => b, 
+          opcode => opcode, 
+          res => res, 
+          zf => zf, 
+          sf => sf,
+          cf => cf
+        );
+    clk_process :process
+    begin
+        clk <= '0'; wait for clock_period/2;
+        clk <= '1'; wait for clock_period/2;
+    end process;
+    stim_proc: process
+    begin		
+        a <= "11111111"; b <= "11111111";
+        opcode <= "0000";
+        wait for 200 ps;
 
-case opcode is
-when "0000" => 
-tempRes <= a * b;
-when "0001" => 
-tempRes <= "00000000" & (a + b);
-when "0010" => 
-tempRes <= "00000000" & (a - b);
-when "0011" => 
-tempRes <= "00000000" & (a and b);
-when "0111" => 
-tempRes <= "00000000" & (a or b);
-when "0110" =>
-tempRes <= "00000000" & (a nand b); 
-when "0100" =>
-tempRes <= "00000000" & (a xor b);
-when "0101" =>
-tempRes <=  "00000000" & (a + 1);
-when "1000" =>
-tempRes <= "00000000" & (a - 1);
-when "1001" =>
-tempRes <= "00000000" & (b - 1);
-when "1010" =>
-tempRes <= "00000000" & (b + 1);
-when others =>
-tempRes <= "0000000000000000";
-end case;
-end process;
+        a <= "00001010"; b <= "00010100";
+        opcode <= "0001";
+        wait for 200 ps;
 
-res <= tempRes;
-sf <= tempRes(15);
+        a <= "00001111"; b <= "00001111";
+        opcode <= "0010";
+        wait for 200 ps;
 
-process(tempRes)
-begin
-if(tempRes = "0000000000000000") then
-zf <= '1';
-else
-zf <= '0';
-end if;
-end process;
+        a <= "11110000"; b <= "10101010";
+        opcode <= "0011";
+        wait for 200 ps;
 
-end Behaviour;
+        a <= "11110000"; b <= "00001111";
+        opcode <= "0111";
+        wait for 200 ps;
+
+        a <= "11111111"; b <= "11111111";
+        opcode <= "0110";
+        wait for 200 ps;
+
+        a <= "10101010"; b <= "01010101";
+        opcode <= "0100";
+        wait for 200 ps;
+
+        a <= "00000101";
+        opcode <= "0101";
+        wait for 200 ps;
+
+        a <= "00000101";
+        opcode <= "1000";
+        wait for 200 ps;
+
+        b <= "00001010";
+        opcode <= "1001";
+        wait for 200 ps;
+
+        b <= "00001010";
+        opcode <= "1010";
+        wait for 200 ps;
+
+        opcode <= "1111";
+        wait for 200 ps;
+    end process;
+
+end Behavior;
