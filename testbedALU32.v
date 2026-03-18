557b248951d3bcd67735a00b79bcf99f63d5c417
Noob_guy@69 <147694315+lol-coder69@users.noreply.github.com>
2026-03-19 03:40:02 +0530

    Update opcode width from 4 to 6 bits in testbench

--- testbedALU32.v
+++ testbedALU32.v
@@ -1,11 +1,9 @@
-
-`timescale 1ns / 1ps
+`timescale 1ps/1ps
 
 module tb_alu_32bit();
-
     reg [31:0] a;
     reg [31:0] b;
-    reg [3:0] opcode;
+    reg [5:0] opcode;
 
     wire [63:0] res;
     wire zf, sf, cf;
@@ -25,24 +23,24 @@ module tb_alu_32bit();
         // TEST 1: Subtraction (Checking Borrow/Less Than)
         a = 32'd100; 
         b = 32'd200;
-        opcode = 4'b0010; // SUB
-        #20; 
+        opcode = 6'b00_1000; // SUB
+        #200; 
         // TEST 2: Logical Shift Left (LSL)
         a = 32'h00000001; // Decimal 1
         b = 32'd4;        // Shift by 4
-        opcode = 4'b1011; // LSL
-        #20; 
+        opcode = 6'b11_1100; // LSL
+        #200; 
         // TEST 3: Arithmetic Shift Right (ASR)
         // Shifting a negative number (MSB is 1)
         a = 32'hFFFF0000; 
         b = 32'd4;        // Shift by 4
-        opcode = 4'b1101; // ASR
-        #20; 
+        opcode = 6'b10_1010; // ASR
+        #200; 
         // TEST 4: Rotate Right (ROR)
         a = 32'h00000005; // Binary: ...0101
         b = 32'd1;        // Rotate right by 1
-        opcode = 4'b1110; // ROR
-        #20; 
+        opcode = 6'b11_1001; // ROR
+        #200; 
         // Result: 0x80000002
 
         $finish;
