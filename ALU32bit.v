1c096dbad68afffdd0781d3c0ad4bdea7815a26c
Noob_guy@69 <147694315+lol-coder69@users.noreply.github.com>
2026-03-21 00:37:53 +0530

    verilog code for a 32 bit alu with multiplication updates
    Added support for 64 bitresult in multiplication and adjusted on operations. Updated default result value and zero flag calculation.

--- ALU32bit.v
+++ ALU32bit.v
@@ -3,6 +3,7 @@ input [31:0] a,
 input [31:0] b,
 input [5:0] opcode,
 output reg [31:0]res,
+output reg [31:0]res_High,
 output reg zf, //zero flag
 output reg sf, //sign flag
 output reg cf, //carry flag
@@ -12,11 +13,28 @@ output reg eq, //equal to
 integer i
 );
 
+wire [63:0] Product;
+wire [63:0] Sn_Product;
+assign Product = (a*b);
+assign Sn_Product = ($signed(a)*$signed(b));
+
 always @(*)begin
 cf = 1'b0;
+lt = 1'b0;
+gt = 1'b0;
+eq = 1'b0;
+res_High = 32'd0;
+
 case(opcode)
 //arithmetic operations
-6'b00_0000://opcode for multiplication
+6'b00_0000:begin
+res = Product[31:0];
+res_High = Product[63:32];
+end
+6'b00_0001:begin
+res = Sn_Product[31:0];
+res_High = Sn_Product[63:32];
+end
 6'b00_0100:res = {a+b};
 6'b00_1000:begin
 res = {a-b};
@@ -35,19 +53,11 @@ end
 6'b01_1000:res = {~(a&b)};
 6'b01_0000:res = {a^b};
 
-//comparison operations
-6'b10_1100:eq = (a == b)?
-6'b11_0000:gt = (a > b)?
-6'b11_0100:lt = (a < b)?
-6'b10_0010:gt = ($signed(a) < $signed(b))?
-6'b10_0111:lt = ($signed(a) > $signed(b))?
-6'b11_1000:eq = ($signed(a) == $signed(b))?
-
 //shift operations
-6'b11_1100:res = {32'd0, a<<b[4:0]};
-6'b10_0001:res = {32'd0, a>>b[4:0]};
-6'b10_1001:res = {32'd0, b<<a[4:0]};
-6'b00_0001:res = {32'd0, b>>a[4:0]};
+6'b11_1100:res = {a<<b[4:0]};
+6'b10_0001:res = {a>>b[4:0]};
+6'b10_1001:res = {b<<a[4:0]};
+6'b00_0001:res = {b>>a[4:0]};
 
 //arithmetic shift operations
 6'b10_1010:res = {$signed(a)>>>b[4:0]};
@@ -62,10 +72,10 @@ end
 6'b10_1001:res = {(b<<a[4:0])|(b>>(32 - a[4:0]))};
 
 //default case
-default:res = 32'd0;
+default:res = 31'd0;
 
 endcase
-  zf = (res == 32'd0)?1'b1:1'b0;
-  sf = res[31];
+zf = (res == 31'd0)?1'b1:1'b0;
+sf = res[31];
 end 
 endmodule
