01bc7bbd658e7c775c0426cc458e9d5be0b588a7
Noob_guy@69 <147694315+lol-coder69@users.noreply.github.com>
2026-03-20 22:43:56 +0530

    Implement Half Adder in VHDL

---
+++ halfAdder.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Half_adder is 
+Port(
+   a:in STD_LOGIC;
+   b:in STD_LOGIC;
+   sum:out STD_LOGIC;
+   carry:out STD_LOGIC
+);
+end Half_adder;
+
+architecture Dataflow of Half_adder is
+begin 
+   sum <= a xor b;
+   carry <= a and b;
+end Dataflow;
