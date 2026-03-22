6e156dc848f48d41b1130cec3402e9852bfa3fdc
Noob_guy@69 <147694315+lol-coder69@users.noreply.github.com>
2026-03-22 23:30:18 +0530

    Fix comments and formatting in FullAdder.vhd

--- FullAdder.vhd
+++ FullAdder.vhd
@@ -13,7 +13,7 @@ c_out:out STD_LOGIC
 end fullAdder;
 
 architecture Structural of fullAdder is
-component Half_Adder //implements the Half_adder entity from halfadder.vhd
+component Half_Adder --implements the Half_adder entity from halfadder.vhd
 Port(
 a:in STD_LOGIC;
 b:in STD_LOGIC;
@@ -25,20 +25,20 @@ end component;
 signal s1:STD_LOGIC;
 signal c1:STD_LOGIC;
 signal c2:STD_LOGIC;
-//implements 2 half adder logic circuits for the full adder logic
+--implements 2 half adder logic circuits for the full adder logic
 begin
 HA_1:Half_Adder Port Map(
 a => a,
 b => b,
 sum => s1,
 carry => c1
-); //1st half adder 
+); --1st half adder 
 HA_2:Half_Adder Port Map(
 a => s1,
 b => c,
 sum => sum,
 carry => c2
-);//2nd half adder
+);--2nd half adder
 
 c_out <= c1 or c2;
 
